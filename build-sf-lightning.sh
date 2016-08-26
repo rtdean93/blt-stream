66f387fe8760e1f70a74786df75108994a539802
Nik Gregory <nikgregory@alum.mit.edu>
2016-08-26 11:26:42 -0400

    1st attemp at a saml lighting distro

---
+++ build-sf-lightning.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+set -o xtrace
+set -e
+
+# Builds some version of d8 with acsf initialized
+DRUSH_PATH="./vendor/bin"
+DRUSH_VERSION='8.1.3'
+
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+install_composer () {
+  echo "Installing composer"
+  mkdir -p bin
+  curl -sS https://getcomposer.org/installer | php -- --install-dir=bin --filename=composer
+}
+
+install_drush () {
+  echo "Installing drush"
+  php bin/composer require drush/drush:${DRUSH_VERSION:=8.1.3}
+}
+
+check_drush_version () {
+  DRUSH_VERSION=`$DRUSH_PATH/drush --version --pipe`
+  if ! [[ $DRUSH_VERSION =~ ^[8]\.[0-9].+$ ]]; then
+  echo "You must be running drush 8 for d8 to work"
+  exit 1
+  fi
+}
+
+install_d8 () {
+  echo "Building drupal 8"
+  # clean up
+  rm -f docroot/composer.lock
+  php bin/composer install -d docroot
+  cd docroot/modules
+  find . -type d -name '.git' | xargs rm -rvf
+  cd -
+  git rm --cached -r docroot/modules/
+}
+
+init_acsf () {
+  echo "Initializing acsf"
+  ./vendor/bin/drush --root=$(pwd)/docroot --include=$(pwd)/docroot/modules/contrib/acsf/acsf_init -y acsf-init
+}
+
+commit_changes () {
+  echo "Committing changes"
+  DATE=$(date)
+  git add -A && git commit -m "Committing build for $DATE"
+  git push origin
+}
+
+install_composer
+install_drush
+check_drush_version
+install_d8
+init_acsf
+if [[ -n $COMMIT_CHANGES ]]; then
+  commit_changes
+else
+  echo "Any changes are not being committed please check the git status output"
+  git status
+fi
